8bb13b8568110fa977b097d8601f66b420b67dc3
Eric Shanks <iconicdesignsystem@gmail.com>
2025-10-08 05:39:49 -0400

    Implement basic button sizing.

--- projects/angular-iconic/src/lib/button/button.ts
+++ projects/angular-iconic/src/lib/button/button.ts
@@ -1,12 +1,22 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, input } from '@angular/core';
 
 @Component({
   selector: 'button[iconic-button]',
   imports: [],
   templateUrl: './button.html',
   styleUrl: './button.scss',
-  host: {
-    'class': 'iconic-button'
-  },
 })
-export class IconicButton {}
+export class IconicButton {
+
+ @HostBinding('class')
+  get hostClasses() {
+    return {
+      'iconic-button': true,
+      [`iconic-button-${this.size()}`]: true
+    };
+  }
+
+  size = input<'small' | 'medium' | 'large'>('medium');
+  variant = input<'neutral' | 'primary' | 'secondary' | 'teritary'>('neutral');
+  apperance = input<'accent' | 'outlined' | 'basic'>('accent');
+}
